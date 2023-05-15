c24bf081a655b230256cec5a3ae2b0ba0c5c57c4
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2023-05-15 14:35:44 +0300

    Mesto - the first edits

--- scripts/script.js
+++ scripts/script.js
@@ -4,25 +4,27 @@ let popUpClose = document.querySelector('.popup__close');
 let formElement = document.querySelector('.form');
 let nameInput = document.querySelector('.form__name');
 let jobInput = document.querySelector('.form__job');
+let formButton = document.querySelector('.form__button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function openPopUp() {
-    popUp.classList.add('popup_opened');
-}  
-
-function closePopUp() {
-    popUp.classList.remove('popup_opened');
+  popUp.classList.add('popup_opened');
 }
 
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    let inputName = nameInput.value;
-    let inputJob = jobInput.value;
-    profileTitle.TextContent = inputName;
-    profileSubtitle = inputJob;
+function closePopUp() {
+  popUp.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInputValue = nameInput.value;
+  let jobInputValue = jobInput.value;
+  profileTitle.TextContent = inputName;
+  profileSubtitle = inputJob;
 }
 
 profileEditButton.addEventListener('click', openPopUp);
 popUpClose.addEventListener('click', closePopUp);
-formElement.addEventListener('submit', handleFormSubmit); 
+formElement.addEventListener('submit', handleFormSubmit);
+formButton.addEventListener('click', closePopUp);
