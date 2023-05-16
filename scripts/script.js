0b2f26c0d383d64e1e7877ca39e37f17820e8d0a
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2023-05-16 14:27:58 +0300

    Mesto - the forth sprint - the second review

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,33 @@
 let profileEditButton = document.querySelector('.profile__edit-button');
-let popUp = document.querySelector('.popup');
-let popUpClose = document.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close');
 let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.form__name');
-let jobInput = document.querySelector('.form__job');
+let nameInput = document.querySelector('input[name="name"]');
+let jobInput = document.querySelector('input[name="job"]');
 let formButton = document.querySelector('.form__button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
-function openPopUp() {
-  popUp.classList.add('popup_opened');
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
-function closePopUp() {
-  popUp.classList.remove('popup_opened');
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   let nameInputValue = nameInput.value;
   let jobInputValue = jobInput.value;
-  profileTitle.TextContent = inputName;
-  profileSubtitle = inputJob;
+
+  profileTitle.TextContent = nameInputValue;
+  profileSubtitle.TextContent = jobInputValue;
+
+  closePopup()
 }
 
-profileEditButton.addEventListener('click', openPopUp);
-popUpClose.addEventListener('click', closePopUp);
-formElement.addEventListener('submit', handleFormSubmit);
-formButton.addEventListener('click', closePopUp);
+profileEditButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
