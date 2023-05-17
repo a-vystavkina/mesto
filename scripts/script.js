2c194c7075b1240367586f2c953c5d1f36f96852
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2023-05-17 10:45:33 +0300

    Mesto - the forth sprint - the third review

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,13 @@ let popupClose = document.querySelector('.popup__close');
 let formElement = document.querySelector('.form');
 let nameInput = document.querySelector('input[name="name"]');
 let jobInput = document.querySelector('input[name="job"]');
-let formButton = document.querySelector('.form__button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function openPopup() {
-  popup.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
+  popup.classList.add('popup_opened');
 }
 
 function closePopup() {
@@ -20,14 +19,13 @@ function closePopup() {
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  let nameInputValue = nameInput.value;
-  let jobInputValue = jobInput.value;
 
-  profileTitle.TextContent = nameInputValue;
-  profileSubtitle.TextContent = jobInputValue;
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
 
   closePopup()
 }
 
 profileEditButton.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
